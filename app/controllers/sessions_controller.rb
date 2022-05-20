a81171e1314f94accf76e4e7fd57863edc7e37ce
kate-hildebrand <kate.hildebrand@kellogg.northwestern.edu>
2022-05-20 20:13:12 +0000

    midpoint

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,24 +2,28 @@ class SessionsController < ApplicationController
 
     def new
     end
-
+  
     def create
-        # step 1: check to see if if there's a user with email that was provided 
-        @user = User.find_by ({ "email" => params["email"]})
-        if @user 
-        # a if yes, check pw (go to step 2)        
-        # 2 check the pw to see if it matches
-        if params["password"] == @user["password"]
-        # if yes, proceed to page 
-            redirect_to "/companies"
+      # Step 1: check to see if there's a user with the email that was provided
+      @user = User.find_by({ "email" => params["email"] })
+      if @user
+        # Step 1a: if yes, check the password (go to Step 2)
+        # Step 2: check the password to see if it matches the user's password
+        if BCrypt::Password.create(@user["password"]) == params["password"]
+          # Step 2a: if yes, go to the companies page
+          flash["notice"] = "You're in!"
+          redirect_to "/companies"
         else
-            # if no, back to login page 
-            redirect_to "/sessions/new"
+          # Step 2b: if no, go back to the login page
+          flash["notice"] = "Incorrect credentials"
+          redirect_to "/sessions/new"
         end
-    else
+      else
         # Step 1b: if no, go back to the login page
         flash["notice"] = "Incorrect credentials"
         redirect_to "/sessions/new"
-    end 
+      end
     end
-end
+  
+  end
+  
--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -6,3 +6,15 @@
 # 1. encrypt a secret and puts encrypted string
 # 2. decrypt the encrypted string
 # 3. check if decrypted value matches secret
+
+secret = "tacos"
+encrypted_secret = BCrypt::Password.create(secret)
+# we will get a different output each time we do rails runner on this 
+puts encrypted_secret
+
+decrypted_secret = BCrypt::Password.new(encrypted_secret)
+
+puts decrypted_secret
+
+puts "Is the password 'tacos'?"
+puts decrypted_secret == "tacos"
